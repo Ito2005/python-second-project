59c17acf8f5da3a0b2498c28d6bc4072b1046a61
LeonardoJr96 <leo70x7000@gmail.com>
2023-11-12 11:18:38 -0300

    create new files and sync in index_biblioteca

--- index_biblioteca.py
+++ index_biblioteca.py
@@ -1,3 +1,15 @@
 from assets.module import arquive
 from assets.module import arquive_zip
+from assets.module import arquive_convert
+from assets.module import arquive_email
+from assets.module import arquive_visualization
+
+from assets.module.create import arquive_template_pptx
+from assets.module.create import arquive_template_xlsx
+
+from assets.module.edit import arquive_word
+from assets.module.edit import arquive_pptx
+from assets.module.edit import arquive_txt
+from assets.module.edit import arquive_xlsx
+
 from assets.biblioteca import *
